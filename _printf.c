6dc7711f1bb9cda11ca890c1c9764155d724b425
Gladys Godwin <gladysgodwin79@gmail.com>
2022-04-22 09:04:44 -0700

    betty correct

--- _printf.c
+++ _printf.c
@@ -12,44 +12,43 @@
 
 int _printf(const char *format, ...)
 {
-    if (format != NULL)
-    {
-        int count = 0, i;
-        int (*d)(va_list);
-        va_list args;
+	if (format != NULL)
+	{
+		int count = 0, i;
+		int (*d)(va_list);
+		va_list args;
 
-        va_start(args, format);
-        i = 0;
-        if (format[0] == '%' && format[1])
-        return (-1);
-        while (format != NULL && format[i] != '\0')
-        {
-            if (format[i] == '%')
-            {
-                if (format[i + 1] == '%')
-                {
-                    count += _putchar(format[i]);
-                    i += 2;
-                }
-                else
-                {
-                    d = func_getter(format[i + 1]);
-                    if (d)
-                        count += d(args);
-                    else
-                        count = _putchar(format[i]) + _putchar(format[i + 1]);
-                    i += 2;
-                }
-            }
-            else
-            {
-                count += _putchar(format[i]);
-                i++;
-            }
-        }
-        va_end(args);
-        return (count);
-    }
-    return (-1);
+		va_start(args, format);
+		i = 0;
+		if (format[0] == '%' && format[1])
+			return (-1);
+		while (format != NULL && format[i] != '\0')
+		{
+			if (format[i] == '%')
+			{
+				if (format[i + 1] == '%')
+				{
+					count += _putchar(format[i]);
+					i += 2;
+				}
+				else
+				{
+					d = func_getter(format[i + 1]);
+					if (d)
+						count += d(args);
+					else
+						count = _putchar(format[i]) + _putchar(format[i + 1]);
+					i += 2;
+				}
+			}
+			else
+			{
+				count += _putchar(format[i]);
+				i++;
+			}
+		}
+		va_end(args);
+		return (count);
+	}
+	return (-1);
 }
-
